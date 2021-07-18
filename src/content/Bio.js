7b12c331d1dfb03e8635c11dea3ec29dbdc9e5ed
Keith Frazier <keithers98@gmail.com>
2021-07-18 12:13:09 -0700

    started  bio and styling

---
+++ src/content/Bio.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Bio.css";
+
+function Bio() {
+  const contacts = <Link className="bioLink">contact</Link>;
+
+  const bioParagraph = `Greetings! I am a full-stack developer graduated from
+    Thinkful's software engineering course as of July 2021. With a long
+    time interest in learning all things computer, web development
+    has been a natural interest of mine. Check out my projects and stack
+    below, and feel free to reach out via my `;
+
+  return (
+    <div className="row justify-content-center bioDiv">
+      <div className="col-6 bioPgraph">
+        {bioParagraph}
+        {contacts}
+        {` links.`}
+      </div>
+    </div>
+  );
+}
+
+export default Bio;
--- src/main/Home.js
+++ src/main/Home.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Routes from "./Routes"
 import Header from "./Header"
+import Bio from "../content/Bio"
 
 function Home() {
  return (
    <main className="container-fluid" style={{padding:"0"}}>
      <Header/>
      <Routes/>
+     <Bio/>
    </main>
  )
 }
