be43531d62072318a03c43e0fc3552f4b7772af7
Keith Frazier <keithers98@gmail.com>
2021-09-22 14:22:26 -0700

    update git ignore

--- src/content/Puzzle.js
+++ src/content/Puzzle.js
@@ -1,165 +1,142 @@
 import React, { useEffect, useState } from "react";
-import "./Puzzle.css";
+import ArrowKeysReact from "arrow-keys-react";
+import "./Grid.css";
+import {
+  generateNeighbors,
+  genPath,
+  genItems,
+  setGrid,
+  params,
+} from "./utils/Grid";
 
 function Puzzle() {
-  const [size, setSize] = useState(9 * 9);
-  const [items, setItems] = useState(genItems(size));
-  const [switcher, setSwitcher] = useState(0);
-  const [axis, setAxis] = useState("");
-  const [neighbors, setNeighbors] = useState([]);
-  const [path, setPath] = useState([0])
-
-  function generateNeighbors() {
-    let result = [];
-    let axisLength = Math.sqrt(size);
-
-    for (let i = 0; i < size; i++) {
-      let currentNeighbors = [];
-      //top
-      i - axisLength > -1
-        ? currentNeighbors.push(i - axisLength)
-        : currentNeighbors.push(null);
-
-      //right
-      i + 1 < size ? currentNeighbors.push(i + 1) : currentNeighbors.push(null);
-
-      //bottom
-      i + axisLength < size
-        ? currentNeighbors.push(i + axisLength)
-        : currentNeighbors.push(null);
-
-      //left
-      i - 1 > -1 ? currentNeighbors.push(i - 1) : currentNeighbors.push(null);
-
-      //push neighbors to result
-      result.push(currentNeighbors);
-    }
-    return result;
-  }
+  const [puzParams, setPuzParams] = useState({});
+  const [paramsSet, setParamsSet] = useState(false);
 
   useEffect(() => {
-    setGrid();
-    setNeighbors(generateNeighbors());
-
-  }, [switcher]);
+    if (!paramsSet) {
+      console.log("setting params, current:",puzParams)
+      setPuzParams({
+        size: 9 * 9,
+        items: [genItems(9 * 9)],
+        switcher: 0,
+        axis: setGrid(81, "50px") ,
+        neighbors: [],
+        path: [0],
+        boardIsSet: false,
+      });
+      setParamsSet(true);
+    }
+  }, [paramsSet]);
 
   useEffect(() => {
-    if (neighbors.length) {
-      setPath(genPath());
+    if (paramsSet) {
+      console.log(puzParams.size, puzParams.axis, "params set, setting grid");
+      ;
+      //setPuzParams({ ...puzParams, axis: setGrid(puzParams.size, "50px") }),
+
+      //setAxis(setGrid(size, "50px"));
+      setPuzParams({
+        ...puzParams,
+        neighbors: generateNeighbors(puzParams.size),
+      });
+      //setNeighbors(generateNeighbors(size));
     }
-  }, [neighbors]);
+  }, [puzParams.switcher, paramsSet]);
 
-  useEffect(()=>{
-      if (path.length){
-        setItems(genItems(size, path));
+  useEffect(() => {
+    console.log("setting path")
+    if (paramsSet && puzParams.neighbors.length && !puzParams.boardIsSet) {
+      setPuzParams({
+        ...puzParams,
+        path: genPath(puzParams.size, puzParams.neighbors),
+      });
+      // setPath(genPath(size, neighbors));
+      //setPuzParams({ ...puzParams, ["setBoardIsSet"]: true });
+      // setBoardIsSet(true);
+    }
+  }, [puzParams.neighbors, paramsSet]);
 
-      }
+  useEffect(() => {
+    console.log(
+      "setting items"
+    )
+    if (paramsSet && puzParams.path.length) {
+      setPuzParams({
+        ...puzParams,
+        items: genItems(puzParams.size, puzParams.path, switchEmUp, [
+          "x",
+          "f",
+        ]),
+      });
 
-  },[path])
+      //setItems(genItems(size, path, size, switchEmUp, ["x", "f"]));
+    }
+  }, [puzParams.path, paramsSet]);
 
   function switchEmUp(event) {
-    setSwitcher(event.target.id);
+    setPuzParams({ ...puzParams, ["switcher"]: event.target.id });
+    // setSwitcher(event.target.id);
   }
 
-  function genPath() {
-    /*
-generate a random path array
-    - start at index 0 
-    - pick a random number from the touching array 
-    - move to index of selected number 
-    - repeat until the number is the finish number 
-  */
-    const path = [];
-    const ends = [8,17,26,35,44,53,62,71]
-
-    const genRandom = () => Math.floor(Math.random() * 3);
-    //const getStep = () => ;
-    const nextStep = (num) => {
-        console.log(path)
-      if (num === size -1) return;
-      const random = genRandom();
-      console.log(random);
-      const step = neighbors[num][random];
-      if (step != null && !path.includes(step) && num < step && !ends.includes(step)) {
-        console.log(step, "should be truthy");
-        path.push(step);
-        nextStep(step);
-        return step;
-      } else {
-        console.log(step, "should be falsy");
-        nextStep(num);
+  window.addEventListener(
+    "keydown",
+    function (event) {
+      if (event.defaultPrevented) {
+        return; // Do nothing if the event was already processed
       }
-    };
 
-    nextStep(0);
+      const cursor = document.querySelector("[name=cursor]");
+      console.log(cursor);
+      const nextMove = puzParams.path.slice(0, 1);
 
-    console.log(path);
-    return path;
-  }
-
-  function setGrid() {
-    const gridArr = [];
-
-    for (let i = 0; i < Math.sqrt(size); i++) {
-      gridArr.push("50px");
-    }
-
-    const gridString = gridArr.join(" ");
-
-    setAxis(gridString);
-  }
-
-  function reorderItems(switcher) {}
-
-  function genItems(num, path) {
-    const style = {
-      borderRadius: "50%",
-      alignSelf: "stretch",
-      //backgroundColor: "blue",
-      margin: "3px",
-      padding: "7px",
-      color: "white",
-    };
-
-    return Array.from(Array(num).keys(), (itm, i) => {
-        let color
-        //console.log(path.length)
-        (path.length && path.includes(i))? color = "green" : color ="blue"
-      if (i === 0) {
-        return (
-          <div index={i} id={i} onMouseOver={switchEmUp} style={style, {backgroundColor:{color}}}>
-            S
-          </div>
-        );
-      } else if (i === size - 1) {
-        return (
-          <div index={i} id={i} onMouseOver={switchEmUp} style={style}>
-            F
-          </div>
-        );
-      } else {
-        return (
-          <div index={i} id={i} onMouseOver={switchEmUp} style={style}>
-            {i}
-          </div>
-        );
+      switch (event.key) {
+        case "Down": // IE/Edge specific value
+        case "ArrowDown":
+          const x = puzParams.items[0];
+          puzParams.items[0] = puzParams.items[8];
+          puzParams.items[8] = x;
+          console.log("after", puzParams.items);
+          console.log("down");
+          break;
+        case "Up": // IE/Edge specific value
+        case "ArrowUp":
+          console.log("up");
+          break;
+        case "Left": // IE/Edge specific value
+        case "ArrowLeft":
+          console.log("left");
+          break;
+        case "Right": // IE/Edge specific value
+        case "ArrowRight":
+          console.log("right");
+          break;
+        default:
+          return; // Quit when this doesn't handle the key event.
       }
-    });
-  }
+
+      // Cancel the default action to avoid it being handled twice
+      event.preventDefault();
+    },
+    true
+  );
+
+  console.log(puzParams);
 
   return (
     <div
+      {...ArrowKeysReact.events}
+      tabIndex="1"
       className="puzzleGrid"
       style={{
         display: "grid",
-        gridTemplateColumns: `${axis}`,
-        gridTemplateRows: `${axis}`,
+        gridTemplateColumns: `${puzParams.axis}`,
+        gridTemplateRows: `${puzParams.axis}`,
         columnGap: "10px",
         rowGap: "15px",
       }}
-      id="showoff"
     >
-      {items}
+      {puzParams.items.flat()}
     </div>
   );
 }
